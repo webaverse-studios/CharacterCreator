1c50a056d4e2f931e588d78ce6e52cbe0e022d88
tcm390 <tcm390@nyu.edu>
2023-01-11 20:01:44 -0500

    change dissolve behaviour

--- src/library/effectManager.js
+++ src/library/effectManager.js
@@ -22,13 +22,20 @@ export class EffectManager{
 
   dissolveCustomShader(material) {
 
-    // material.vertexShader = material.vertexShader.replace(
-    //   `varying vec3 vViewPosition;`,
-    //   `
-    //   varying vec3 vViewPosition;
-    //   varying vec3 vWorldPosition;
-    //   `,
-    // );
+    material.vertexShader = material.vertexShader.replace(
+      `varying vec3 vViewPosition;`,
+      `
+      varying vec3 vViewPosition;
+      varying vec3 vWorldPosition;
+      `,
+    );
+    material.vertexShader = material.vertexShader.replace(
+      `#include <worldpos_vertex>`,
+      `
+      #include <worldpos_vertex>
+      vWorldPosition = (modelMatrix * vec4( transformed, 1.0 )).xyz;
+      `,
+    );
     
     material.fragmentShader = material.fragmentShader.replace(
       `uniform vec3 litFactor;`,
@@ -36,6 +43,7 @@ export class EffectManager{
       uniform vec3 litFactor;
       uniform float uTime;
       uniform sampler2D dissolveTexture;
+      varying vec3 vWorldPosition;
       
       float pat(vec2 uv, float p, float q, float s, float glow){
         float z = cos(p * uv.y / 0.5) + cos(q * uv.y / 2.2);
@@ -43,40 +51,54 @@ export class EffectManager{
         float dist = abs(z) * (0.1 / glow);
         return dist;
       }
+      vec4 textureRemap(vec4 In, vec2 InMinMax, vec2 OutMinMax) {
+        return OutMinMax.x + (In - InMinMax.x) * (OutMinMax.y - OutMinMax.x) / (InMinMax.y - InMinMax.x);
+      }
       `,
     );
     material.fragmentShader = material.fragmentShader.replace(
       `diffuseColor *= sampledDiffuseColor;`,
       `
       float noiseUvScale = 0.3;
-      float noise = texture2D(dissolveTexture, mapUv * noiseUvScale).r;
-      float freq = 1.0;
-      float border = 0.05;
-      float limit = abs(cos(uTime * freq));
-      // float limit = 0.5;
-      vec3 boderColor = vec3(0.3, 0., 1.0);
-      vec3 boderColor2 = vec3(0.990, 0.752, 0.851);
+      vec4 noise = texture2D(dissolveTexture, mapUv * noiseUvScale);
 
-      if (noise < limit && noise > limit - border) {
-        float distanceToMid = abs(((limit - noise) / border) - 0.5) * 2.;
+      float noiseStrength = 0.5;
+      float noiseCutout = textureRemap(noise, vec2(0.0, 1.0), vec2(-noiseStrength, noiseStrength)).r;
+
+      float bottomPosition = 0.7;
+      float avatarHeight = 3.1;
+      float speed = 0.7;
+      float cutoutHeight = abs(cos(uTime * speed)) * avatarHeight - bottomPosition;
+      
+      float limit = noiseCutout + cutoutHeight;
+      float border = 0.1;
+      
+      float upperBound = limit + border;
+
+      if (vWorldPosition.y > limit && vWorldPosition.y < upperBound) {
+        vec3 boderColor = vec3(0.3, 0., 1.0);
+        vec3 boderColor2 = vec3(0.990, 0.752, 0.851);
+
+        
+        float distanceToMid = abs((upperBound - vWorldPosition.y) / border - 0.5) * 2.0;
         diffuseColor = vec4(mix(boderColor, boderColor2, 1. - distanceToMid), 1.0);
-
         float d = pat(mapUv * noiseUvScale, 1.0, 2.0, 10.0, 0.35);		
         diffuseColor = diffuseColor * 0.5 / d;
         
         diffuseColor = smoothstep(0.18, 0.5, diffuseColor);
+        
       }
       else {
         diffuseColor *= sampledDiffuseColor;
-        diffuseColor.a = step(limit - border, noise);
+        diffuseColor.a = step(limit, vWorldPosition.y);
       }
       `,
     );
-    
+
     material.fragmentShader = material.fragmentShader.replace(
       `col += totalEmissiveRadiance;`,
       `
-      if (noise < limit && noise > limit - border) {
+      if (vWorldPosition.y > limit && vWorldPosition.y < upperBound) {
         col = diffuseColor.rgb;
       }
       else {
@@ -85,6 +107,47 @@ export class EffectManager{
       
       `,
     );
+
+    // material.fragmentShader = material.fragmentShader.replace(
+    //   `diffuseColor *= sampledDiffuseColor;`,
+    //   `
+    //   float noiseUvScale = 0.3;
+    //   float noise = texture2D(dissolveTexture, mapUv * noiseUvScale).r;
+    //   float freq = 1.0;
+    //   float border = 0.05;
+    //   float limit = abs(cos(uTime * freq));
+    //   // float limit = 0.5;
+    //   vec3 boderColor = vec3(0.3, 0., 1.0);
+    //   vec3 boderColor2 = vec3(0.990, 0.752, 0.851);
+
+    //   if (noise < limit && noise > limit - border) {
+    //     float distanceToMid = abs(((limit - noise) / border) - 0.5) * 2.;
+    //     diffuseColor = vec4(mix(boderColor, boderColor2, 1. - distanceToMid), 1.0);
+
+    //     float d = pat(mapUv * noiseUvScale, 1.0, 2.0, 10.0, 0.35);		
+    //     diffuseColor = diffuseColor * 0.5 / d;
+        
+    //     diffuseColor = smoothstep(0.18, 0.5, diffuseColor);
+    //   }
+    //   else {
+    //     diffuseColor *= sampledDiffuseColor;
+    //     diffuseColor.a = step(limit - border, noise);
+    //   }
+    //   `,
+    // );
+    
+    // material.fragmentShader = material.fragmentShader.replace(
+    //   `col += totalEmissiveRadiance;`,
+    //   `
+    //   if (noise < limit && noise > limit - border) {
+    //     col = diffuseColor.rgb;
+    //   }
+    //   else {
+    //     col += totalEmissiveRadiance;
+    //   }
+      
+    //   `,
+    // );
 
     
     
