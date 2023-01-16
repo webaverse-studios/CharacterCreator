aa8aa00d17b68933aef6d5dbcaa89d1b0c160410
tcm390 <tcm390@nyu.edu>
2023-01-15 19:48:45 -0500

    increase speed

--- src/library/effectManager.js
+++ src/library/effectManager.js
@@ -34,7 +34,7 @@ export class EffectManager{
     this.fadeOut = false;
     
     this.frameRate = 1000 / 30;
-    this.fadeSpeed = 0.02;
+    this.fadeSpeed = 0.03;
     this.fadeCycle = this.frameRate * ((fadeInThreshold - fadeOutThreshold) / this.fadeSpeed);
 
     this.update();
@@ -92,74 +92,78 @@ export class EffectManager{
     material.fragmentShader = material.fragmentShader.replace(
       `diffuseColor *= sampledDiffuseColor;`,
       `
-      
-      
-      float noiseUvScale = 3.5;
-      vec2 noiseUv = vec2(
-        vWorldPosition.x * noiseUvScale * -cameraDir.z + vWorldPosition.z * noiseUvScale * cameraDir.x,
-        vWorldPosition.y * noiseUvScale - uTime * noiseUvScale
-      );
-      vec4 noise = texture2D(
-        dissolveTexture, 
-        noiseUv
-      );
-
-      float fadeOutStrength = 0.2;
-      float fadeInStrength = 0.025;
-      float minStrength = fadeOut ? fadeOutStrength : fadeInStrength;
-      float noiseStrength = uTime < 1.? pow(1. - uTime, 5.0) + minStrength : minStrength;
-      float noiseCutout = textureRemap(noise, vec2(0.0, 1.0), vec2(-noiseStrength, noiseStrength)).r;
-
-      float bottomPosition = 1.1;
-      float avatarHeight = 4.0;
-      float speed = 0.7;
-      float cutoutHeight = ((uTime * speed)) * avatarHeight - bottomPosition;
-      
-      float limit = noiseCutout + cutoutHeight;
-      float border = 0.075;
-      
-      float upperBound = limit + border;
-
-      vec3 boderColor = vec3(0.0, 0.1, 1.0);
-      vec3 boderColor2 = vec3(0.0864, 0.844, 0.960);
-
-      vec3 eyeDirection = normalize(eye - vWorldPosition);
-      float grid = texture2D(
-        dissolveTexture, 
-        vec2(
+      bool isBorder = false;
+      if (uTime < 1.0) {
+        float noiseUvScale = 3.5;
+        vec2 noiseUv = vec2(
           vWorldPosition.x * noiseUvScale * -cameraDir.z + vWorldPosition.z * noiseUvScale * cameraDir.x,
-          vWorldPosition.y * noiseUvScale
-        )
-      ).r;
-      float gridIntensity = 10.0;
-      float EdotN = max(0.0, dot(eyeDirection, normalize(vSurfaceNormal + vec3(grid) * vec3(1.0, gridIntensity * (1. - uTime), 1.0))));
-      
-      bool isBorder = vWorldPosition.y > limit && vWorldPosition.y < upperBound;
-      
-      if (isBorder && !fadeOut) {
-        float rimStrength = 0.5 * uTime;
-        float boaderRim = mix(0.0, 1.0, pow(1. - EdotN, rimStrength));
-        float glowIntensity = clamp(100. * (1. - uTime), 20., 100.);
+          vWorldPosition.y * noiseUvScale - uTime * noiseUvScale
+        );
+        vec4 noise = texture2D(
+          dissolveTexture, 
+          noiseUv
+        );
 
-        float distanceToMid = abs((upperBound - vWorldPosition.y) / border - 0.5) * 2.0;
-        diffuseColor = vec4(mix(boderColor, boderColor2, 1. - distanceToMid), 1.0);
-        float d = pat(noiseUv, 1.0, 2.0, 10.0, 0.35);		
-        diffuseColor = diffuseColor * 0.9 / d;
+        float fadeOutStrength = 0.2;
+        float fadeInStrength = 0.025;
+        float minStrength = fadeOut ? fadeOutStrength : fadeInStrength;
+        float noiseStrength = uTime < 1.? pow(1. - uTime, 5.0) + minStrength : minStrength;
+        float noiseCutout = textureRemap(noise, vec2(0.0, 1.0), vec2(-noiseStrength, noiseStrength)).r;
+
+        float bottomPosition = 1.1;
+        float avatarHeight = 4.0;
+        float speed = 0.7;
+        float cutoutHeight = ((uTime * speed)) * avatarHeight - bottomPosition;
         
-        diffuseColor = smoothstep(0., 0.5, diffuseColor);
+        float limit = noiseCutout + cutoutHeight;
+        float border = 0.1;
+        
+        float upperBound = limit + border;
 
-        // vec3 boderRimColor = mix(boderColor, boderColor2, noiseCutout);
-        diffuseColor.rgb *= boaderRim * glowIntensity;
+        vec3 boderColor = vec3(0.0, 0.1, 1.0);
+        vec3 boderColor2 = vec3(0.0864, 0.844, 0.960);
+
+        vec3 eyeDirection = normalize(eye - vWorldPosition);
+        float grid = texture2D(
+          dissolveTexture, 
+          vec2(
+            vWorldPosition.x * noiseUvScale * -cameraDir.z + vWorldPosition.z * noiseUvScale * cameraDir.x,
+            vWorldPosition.y * noiseUvScale
+          )
+        ).r;
+        float gridIntensity = 10.0;
+        float EdotN = max(0.0, dot(eyeDirection, normalize(vSurfaceNormal + vec3(grid) * vec3(1.0, gridIntensity * (1. - uTime), 1.0))));
+        
+        isBorder = vWorldPosition.y > limit && vWorldPosition.y < upperBound;
+        if (isBorder && !fadeOut) {
+          float rimStrength = 0.5 * uTime;
+          float boaderRim = mix(0.0, 1.0, pow(1. - EdotN, rimStrength));
+          float glowIntensity = clamp(100. * (1. - uTime), 20., 100.);
+  
+          float distanceToMid = abs((upperBound - vWorldPosition.y) / border - 0.5) * 2.0;
+          diffuseColor = vec4(mix(boderColor, boderColor2, 1. - distanceToMid), 1.0);
+          float d = pat(noiseUv, 1.0, 2.0, 10.0, 0.35);		
+          diffuseColor = diffuseColor * 0.9 / d;
+          
+          diffuseColor = smoothstep(0., 0.5, diffuseColor);
+  
+          // vec3 boderRimColor = mix(boderColor, boderColor2, noiseCutout);
+          diffuseColor.rgb *= boaderRim * glowIntensity;
+        }
+        else {
+          diffuseColor *= sampledDiffuseColor;
+          float rimStrength = 1.5 * uTime;
+          float bodyRim = mix(0.0, 1.0, pow(1. - EdotN, rimStrength));
+          float glowIntensity = fadeOut? 150. * (1. - uTime) : 25. * (1. - uTime);
+  
+          diffuseColor.rgb += boderColor * bodyRim * glowIntensity;
+          diffuseColor.a = step(vWorldPosition.y, limit);
+        }
       }
       else {
         diffuseColor *= sampledDiffuseColor;
-        float rimStrength = 1.5 * uTime;
-        float bodyRim = mix(0.0, 1.0, pow(1. - EdotN, rimStrength));
-        float glowIntensity = fadeOut? 150. * (1. - uTime) : 25. * (1. - uTime);
-
-        diffuseColor.rgb += boderColor * bodyRim * glowIntensity;
-        diffuseColor.a = step(vWorldPosition.y, limit);
       }
+      
       
       
       // if (fadeOut) {
@@ -177,13 +181,12 @@ export class EffectManager{
     material.fragmentShader = material.fragmentShader.replace(
       `col += totalEmissiveRadiance;`,
       `
-      if (isBorder) {
+      if (uTime < 1.0 && isBorder) {
         col = diffuseColor.rgb;
       }
       else {
         col += totalEmissiveRadiance;
       }
-      
       `,
     );
 
